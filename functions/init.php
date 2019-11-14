4aa41b90ea077fb7fc5df031045d1801233b6b25
Dat Do <30649150+dohdat@users.noreply.github.com>
2019-11-14 15:00:58 -0800

    Initial commit

---
+++ functions/init.php
@@ -0,0 +1,5 @@
+<?php
+include ("db.php");
+include ("functions.php");
+
+?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include ("includes/header.php")?>
+
+<?php include ("includes/nav.php")?>	 
+	
+
+
+
+	<div class="jumbotron">
+		<h1 class="text-center"> Home Page</h1>
+	</div>
+
+
+	
+<?php include ("includes/footer.php")?>
