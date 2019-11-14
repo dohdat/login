5f84d9acb2ad8b63de6400224a8c4fe1109c1f10
Dat Do <30649150+dohdat@users.noreply.github.com>
2019-11-14 15:34:22 -0800

    Establish database connection

--- functions/db.php
+++ functions/db.php
@@ -1,3 +1,30 @@
 <?php
+$con =  mysqli_connect('localhost', 'root','','login_db');
+
+
+function escape($string) {
+	global $con;
+	return mysqli_real_escape_string($con, $string);
+
+}
+//performs a query on the database
+function query($query) {
+	global $con;
+	return mysqli_query($con, $query);
+}
+
+//confirm function
+function confirm($result) {
+	global $con;
+	if(!$result) {
+		die("Query failed!". mysqli_error($con));
+	}
+}
+//grab the result and fetch for us, fetch all data. 
+function fetch_array($result) {
+	global $con;
+	return mysqli_fetch_array($result);
+}
+
 
 ?>
